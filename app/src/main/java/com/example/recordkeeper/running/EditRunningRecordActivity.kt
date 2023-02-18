c529547c5b420ab8924511257f798b37b1da58ec
OgnjenAD <ognjen.milisoft@gmail.com>
2023-02-18 19:04:01 +0100

    190

--- app/src/main/java/com/example/recordkeeper/running/EditRunningRecordActivity.kt
+++ app/src/main/java/com/example/recordkeeper/running/EditRunningRecordActivity.kt
@@ -1,7 +1,6 @@
 package com.example.recordkeeper.running
 
 import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
@@ -10,38 +9,49 @@ import com.example.recordkeeper.databinding.ActivityEditRunningRecordBinding
 class EditRunningRecordActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditRunningRecordBinding
-    private lateinit var runningPreferences: SharedPreferences
-    private val distance: String? by lazy { intent.getStringExtra("Distance") }
+    private val runningPreferences by lazy { getSharedPreferences("running", Context.MODE_PRIVATE) }
+    private val distance by lazy { intent.getStringExtra("Distance") }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditRunningRecordBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        runningPreferences = getSharedPreferences("running", Context.MODE_PRIVATE)
-
-        title = "$distance Record"
-
+        setupUI()
         displayRecord()
 
+    }
+
+    private fun setupUI() {
+        title = "$distance Record"
         binding.btnSave.setOnClickListener {
             saveRecord()
             finish()
         }
-    }
 
+        binding.btnDelete.setOnClickListener {
+            clearRecord()
+            finish()
+        }
+    }
     private fun displayRecord() {
         binding.etRecord.setText(runningPreferences.getString("$distance record", null))
         binding.etDate.setText(runningPreferences.getString("$distance date", null))
     }
-
     private fun saveRecord() {
         val record = binding.etRecord.text.toString()
         val date = binding.etDate.text.toString()
         
         runningPreferences.edit {
             putString("$distance record", record)
-            putString("date", date)
+            putString("$distance date", date)
+        }
+    }
+
+    private fun clearRecord() {
+        runningPreferences.edit {
+            remove("$distance record")
+            remove("$distance date")
         }
     }
 }
